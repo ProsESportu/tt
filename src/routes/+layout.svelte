4c472549550aa38b699a2ab4da3a5a5a118890ba
krkub <krkub2137@gmail.com>
2023-09-06 13:56:04 +0200

    ffsa

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+    import "../app.css";
+    import { DarkMode, } from "flowbite-svelte";
+</script>
+<div class="h-screen bg-white dark:bg-gray-800 flex flex-col">
+    <DarkMode></DarkMode>
+    <slot></slot>
+</div>
