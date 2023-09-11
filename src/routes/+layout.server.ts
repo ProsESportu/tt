ad25e0ac4b27fbe1ddafb5dc8843d8c46d0909ff
ProsESportu <t.hajduga@poczta.fm>
2023-09-11 10:04:16 +0200

    global isr

--- src/routes/+layout.server.ts
+++
@@ -1,9 +0,0 @@
-import { BYPASS_TOKEN } from "$env/static/private";
-import type { Config } from "@sveltejs/adapter-vercel";
-
-export const config:Config={
-    isr:{
-        expiration:3600,
-        bypassToken:BYPASS_TOKEN
-    }
-}
